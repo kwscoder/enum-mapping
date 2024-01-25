e6584479bf2efff514ba96ba80b26a3dde27431f
kws <k.ws@qq.com>
2024-01-25 14:09:50 +0800

    feat: fastjson自定义枚举类Configuration更新

--- enum-mapping-fastjson/src/main/java/com/kws/fastjson/config/FastJsonEnumConfiguration.java
+++ enum-mapping-fastjson/src/main/java/com/kws/fastjson/config/FastJsonEnumConfiguration.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.spi.Module;
 import com.kws.annotation.EnumValueMarkerFinder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -19,26 +20,11 @@ import javax.annotation.PostConstruct;
 @Slf4j
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass({ObjectSerializer.class, ObjectDeserializer.class})
+//@ConditionalOnMissingClass({"com.alibaba.fastjson.serializer.ObjectSerializer"})
 public class FastJsonEnumConfiguration {
-    public FastJsonEnumConfiguration() {
-        log.info("****** constructor ******");
-    }
-
-    @PostConstruct
-    public void init() {
-        log.info("****** init ******");
-        log.debug("====== Register fastjson custom enum serialization and deserialization ======");
-        log.info("====== 加载Fastjson枚举类自定义序列化、反序列化 ======");
-        MyEnumModule customizeEnumModule = new MyEnumModule();
-        SerializeConfig instance = SerializeConfig.getGlobalInstance();
-        instance.register(customizeEnumModule);
-
-        ParserConfig parserConfig = ParserConfig.getGlobalInstance();
-        parserConfig.register(customizeEnumModule);
-    }
 
     /*
-     * 当在class上配置 @ConditionalOnMissingClass 注解时，若指定类存在，则不会执行static代码块代码
+     * 当配置了 @ConditionalOnMissingClass 注解时，若指定类存在，则类不会被加载，即：不会执行static代码块
      */
     static {
         log.info("****** static block ******");
@@ -51,6 +37,23 @@ public class FastJsonEnumConfiguration {
 //        parserConfig.register(customizeEnumModule);
     }
 
+    public FastJsonEnumConfiguration() {
+        log.info("****** constructor ******");
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("****** init ******");
+        log.info("====== Register fastjson custom enum serializer and deserializer ======");
+        MyEnumModule customizeEnumModule = new MyEnumModule();
+        SerializeConfig instance = SerializeConfig.getGlobalInstance();
+        instance.register(customizeEnumModule);
+
+        ParserConfig parserConfig = ParserConfig.getGlobalInstance();
+        parserConfig.register(customizeEnumModule);
+    }
+
+
     @Slf4j
     public static class MyEnumModule implements Module {
         public MyEnumModule() {
@@ -58,18 +61,16 @@ public class FastJsonEnumConfiguration {
 
         @Override
         public ObjectDeserializer createDeserializer(ParserConfig config, Class type) {
-            if (!type.isEnum() || !EnumValueMarkerFinder.hasAnnotation(type)) {
-                return null;
-            }
-            return new FastJsonEnumDeserializer();
+            return isCustomizedEnum(type) ? new FastJsonEnumDeserializer() : null;
         }
 
         @Override
         public ObjectSerializer createSerializer(SerializeConfig config, Class type) {
-            if (!type.isEnum() || !EnumValueMarkerFinder.hasAnnotation(type)) {
-                return null;
-            }
-            return new FastJsonEnumSerializer();
+            return isCustomizedEnum(type) ? new FastJsonEnumSerializer() : null;
+        }
+
+        private boolean isCustomizedEnum(Class<?> type) {
+            return type.isEnum() && EnumValueMarkerFinder.hasAnnotation(type);
         }
     }
 
